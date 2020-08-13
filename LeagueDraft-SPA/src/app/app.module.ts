b8570a316cb7ad94ce9dbf6a5fbc02cc978e47ae
Woolfer <arek.pazola1998@gmail.com>
2020-08-13 12:31:00 +0200

    Add layout for FreeDraft component
    Its still missing logic and is in progress but it show how it will likely looks like

--- LeagueDraft-SPA/src/app/app.module.ts
+++ LeagueDraft-SPA/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import { FooterComponent } from './shared/Components/footer/footer.component';
 import { NavbarComponent } from './shared/Components/navbar/navbar.component';
-import {MatDividerModule} from "@angular/material/divider";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
+import { MatInputModule} from "@angular/material/input";
+import { MatDividerModule} from "@angular/material/divider";
+import { MatToolbarModule} from "@angular/material/toolbar";
+import { MatIconModule} from "@angular/material/icon";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {_MatMenuDirectivesModule, MatMenuModule} from "@angular/material/menu";
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -20,6 +21,10 @@ import {MatExpansionModule} from "@angular/material/expansion";
 import { FreeDraftComponent } from './free-draft/free-draft.component';
 import { LiveDraftComponent } from './live-draft/live-draft.component';
 import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import { ChampionsComponent } from './admin/champions/champions.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @NgModule({
   declarations: [
@@ -29,7 +34,8 @@ import {MatCardModule} from "@angular/material/card";
     DashboardComponent,
     SidebarComponent,
     FreeDraftComponent,
-    LiveDraftComponent
+    LiveDraftComponent,
+    ChampionsComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +51,11 @@ import {MatCardModule} from "@angular/material/card";
     MatSidenavModule,
     MatListModule,
     MatExpansionModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- LeagueDraft-SPA/src/app/routes.ts
+++ LeagueDraft-SPA/src/app/routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {LiveDraftComponent} from "./live-draft/live-draft.component";
 import {FreeDraftComponent} from "./free-draft/free-draft.component";
+import {ChampionsComponent} from "./admin/champions/champions.component";
 
 export const appRoutes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'live-draft', component: LiveDraftComponent },
   { path: 'free-draft', component: FreeDraftComponent },
+  { path: 'admin/champions', component: ChampionsComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
